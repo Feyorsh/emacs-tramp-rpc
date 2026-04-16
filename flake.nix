5a486e0ebdf5df6ebab3dc65f2b8160d58a751ad
Arthur Heymans <arthur@aheymans.xyz>
2026-04-16 14:15:23 +0200

    ci: add i686, armv7, armv5te, and arm/ARMv6 Linux build targets
    Add build-only CI jobs for four additional static musl targets:
    - i686-unknown-linux-musl (32-bit x86)
    - armv7-unknown-linux-musleabihf (Allwinner A20/H3, Cortex-A7)
    - armv5te-unknown-linux-musleabi (Aspeed AST2500, ARM926EJ-S)
    - arm-unknown-linux-musleabihf (original Raspberry Pi, ARM1176JZF-S / ARMv6)
    
    Adds the corresponding Nix cross-compilation toolchains to flake.nix
    and crt-static entries to .cargo/config.toml for local builds.

--- flake.nix
+++ flake.nix
@@ -37,6 +37,32 @@
         let
           pkgs = nixpkgs.legacyPackages.${system};
 
+          # Cross-compilation toolchains for extra Linux targets
+          pkgsCrossI686Musl = pkgs.pkgsCross.musl32;
+          pkgsCrossArmv5teMusl = import nixpkgs {
+            inherit system;
+            crossSystem = lib.systems.elaborate {
+              config = "armv5tel-unknown-linux-musleabi";
+              libc = "musl";
+            };
+          };
+          # ARMv7 hard-float: Allwinner A20/H3 (Cortex-A7), Raspberry Pi 2+
+          pkgsCrossArmv7Musl = import nixpkgs {
+            inherit system;
+            crossSystem = lib.systems.elaborate {
+              config = "armv7l-unknown-linux-gnueabihf";
+              libc = "musl";
+            };
+          };
+          # ARMv6 hard-float: original Raspberry Pi (ARM1176JZF-S)
+          pkgsCrossArmMusl = import nixpkgs {
+            inherit system;
+            crossSystem = lib.systems.elaborate {
+              config = "armv6l-unknown-linux-gnueabihf";
+              libc = "musl";
+            };
+          };
+
           # Nightly toolchain with rust-src for build-std (size-optimized builds)
           rustToolchain = rust-overlay.packages.${system}.rust-nightly.override {
             targets = [
@@ -44,6 +70,10 @@
               "aarch64-unknown-linux-musl"
               "x86_64-apple-darwin"
               "aarch64-apple-darwin"
+              "i686-unknown-linux-musl"
+              "armv7-unknown-linux-musleabihf"
+              "armv5te-unknown-linux-musleabi"
+              "arm-unknown-linux-musleabihf"
             ];
             extensions = [ "rust-src" ];
           };
@@ -56,6 +86,10 @@
               pkgs.pkg-config
               pkgs.pkgsCross.musl64.stdenv.cc
               pkgs.pkgsCross.aarch64-multiplatform-musl.stdenv.cc
+              pkgsCrossI686Musl.stdenv.cc
+              pkgsCrossArmv7Musl.stdenv.cc
+              pkgsCrossArmv5teMusl.stdenv.cc
+              pkgsCrossArmMusl.stdenv.cc
               pkgs.rust-analyzer
             ];
 
@@ -70,6 +104,10 @@
 
               export CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_LINKER="${pkgs.pkgsCross.musl64.stdenv.cc}/bin/x86_64-unknown-linux-musl-gcc"
               export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_LINKER="${pkgs.pkgsCross.aarch64-multiplatform-musl.stdenv.cc}/bin/aarch64-unknown-linux-musl-gcc"
+              export CARGO_TARGET_I686_UNKNOWN_LINUX_MUSL_LINKER="${pkgsCrossI686Musl.stdenv.cc}/bin/${pkgsCrossI686Musl.stdenv.cc.targetPrefix}gcc"
+              export CARGO_TARGET_ARMV7_UNKNOWN_LINUX_MUSLEABIHF_LINKER="${pkgsCrossArmv7Musl.stdenv.cc}/bin/${pkgsCrossArmv7Musl.stdenv.cc.targetPrefix}gcc"
+              export CARGO_TARGET_ARMV5TE_UNKNOWN_LINUX_MUSLEABI_LINKER="${pkgsCrossArmv5teMusl.stdenv.cc}/bin/${pkgsCrossArmv5teMusl.stdenv.cc.targetPrefix}gcc"
+              export CARGO_TARGET_ARM_UNKNOWN_LINUX_MUSLEABIHF_LINKER="${pkgsCrossArmMusl.stdenv.cc}/bin/${pkgsCrossArmMusl.stdenv.cc.targetPrefix}gcc"
             '';
           };
         }
